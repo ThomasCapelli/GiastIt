b53a95669ecab2765ff3d3931f4170b703653125
ThomasCapelli <thomas.capelli@studio.unibo.it>
2022-02-09 16:07:36 +0100

    Creation

---
+++ app/src/main/java/com/example/giastit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.giastit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
